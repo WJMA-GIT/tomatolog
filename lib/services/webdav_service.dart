09d2b2f66e28f21e1902073fd8a4bb208082e173
WJMA-GIT <WJMA-GIT@users.noreply.github.com>
2026-09-19 01:08:32 +0800

    合并双平台发布并重置版本

--- lib/services/webdav_service.dart
+++ lib/services/webdav_service.dart
@@ -121,7 +121,7 @@ class WebDavClient {
           .openUrl(method, _uri(path))
           .timeout(timeout);
       request.headers.set(HttpHeaders.authorizationHeader, _authorization);
-      request.headers.set(HttpHeaders.userAgentHeader, 'tomatolog/0.0.1');
+      request.headers.set(HttpHeaders.userAgentHeader, 'tomatolog/0.0.5');
       headers.forEach(request.headers.set);
       if (body != null) {
         request.contentLength = body.length;
